6a807fdb557661ee51c19155ff68877204906e5a
patrickbyan <patrickbyann@gmail.com>
2021-11-04 01:02:25 +0700

    implement PrintReceipt

--- go.mod
+++ go.mod
@@ -3,5 +3,5 @@ module github.com/patrickbyan/sysacademy_main_project
 go 1.17
 
 require (
-	github.com/patrickbyan/sysacademy_module_project/v2 v2.2.0
+	github.com/patrickbyan/sysacademy_module_project/v2 v2.3.0
 )
--- main.go
+++ main.go
@@ -23,6 +23,13 @@ func main() {
 		UnitId:       unit,
 	}
 
+	var (
+		success    bool
+		units      []string
+		totalPrice int64
+		metaData   int
+	)
+
 	if activeStatus {
 		err, errMessage := newBuyer.UnitEligibilityCheck()
 
@@ -30,11 +37,19 @@ func main() {
 			module.HandleError(err, 4, errMessage, newBuyer.UnitId, 0)
 			// fmt.Printf("error: %v, message: %s \n", err, errMessage)
 		} else {
-			unitNames, totalPrice, metaData := newBuyer.PurchaseUnits()
-			module.HandleError(false, 2, "Purchase success", newBuyer.UnitId, int8(metaData))
-			fmt.Printf("purchase %v units (total: %v data) with a price of %v success \n", unitNames, metaData, totalPrice)
+			unitNames, price, meta := newBuyer.PurchaseUnits()
+			module.HandleError(false, 2, "Purchase success", newBuyer.UnitId, int8(meta))
+
+			success = true
+			units = unitNames
+			totalPrice = price
+			metaData = meta
 		}
 	} else {
 		module.HandleError(true, 4, "Status Inactive", newBuyer.UnitId, 0)
+	}
+
+	if success {
+		module.PrintReceipt(int32(metaData), totalPrice, units...)
 	}
 }
